7326fafbbb88c25ba4d2695425117a675b55feed
ComradeXu <876581792@qq.com>
2017-07-06 14:37:59 +0800

    visa special

---
+++ Project/mobile/visa/js/index.js
@@ -0,0 +1,12 @@
+$(function(){
+	$('.icon-menu').click(function(){
+		if($('.menu-ul').css('display')=='none'){
+			$('.menu-ul').slideDown();
+		}else{
+			$('.menu-ul').slideUp();
+		}
+	});
+	$('.menu-ul ul li a').click(function(){
+		$(this).parents('.menu-ul').slideUp('fast')
+	})
+})
