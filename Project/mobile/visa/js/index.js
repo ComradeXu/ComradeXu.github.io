66a9948bba07899ac613f9fe044468583745e498
ComradeXu <876581792@qq.com>
2017-07-06 18:30:38 +0800

    reverse special

--- Project/mobile/visa/js/index.js
+++ Project/mobile/visa/js/index.js
@@ -1,6 +1,5 @@
 $(function(){
 	$('.icon-menu').click(function(){
-		alert('d');
 		if($('.menu-ul').css('display')=='none'){
 			$('.menu-ul').slideDown();
 		}else{
@@ -9,5 +8,8 @@ $(function(){
 	});
 	$('.menu-ul ul li a').click(function(){
 		$(this).parents('.menu-ul').slideUp('fast')
+	});
+	$('.ddd').click(function(){
+		$('.aaa').show();
 	})
 })
