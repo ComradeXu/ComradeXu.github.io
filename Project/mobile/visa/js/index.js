a866ca5eb59afba1dce328bdaf0e1ce65be614d1
name <email>
2017-07-12 15:52:27 +0800

    菜单弹出后禁止页面滚动

--- Project/mobile/visa/js/index.js
+++ Project/mobile/visa/js/index.js
@@ -28,5 +28,80 @@ $(function(){
 		$('.menu-ul').removeClass('mshow');
 		$('.menu-ul').addClass('mhide');
 
+	});
+	
+	$(".menu-ul").on("swiperight",function(){
+		$('.headerMask').hide();
+		$('.wrapper').removeClass('mhide1');
+		$('.wrapper').removeClass('mshow1');
+		$('.wrapper').addClass('mshow1');
+		$('.wrapper').removeAttr('style');
+		$('.menu-ul').removeClass('mshow');
+		$('.menu-ul').addClass('mhide');
 	})
 }); 
+var iconBtn = $('.icon-menu');
+iconBtn.on('click', function () {
+	$('html').addClass('noscroll');
+});
+$('.headerMask').on('click', function () {
+	$('html').removeClass('noscroll');
+});
+$.smartScroll = function(container, selectorScrollable) {
+	if (!selectorScrollable || container.data('isBindScroll')) {
+		return;
+	}
+	var isSBBrowser;
+	var data = {
+		posY: 0,
+		maxscroll: 0
+	};
+	container.on({
+		touchstart: function (event) {
+			var events = event.touches[0] || event;
+			var elTarget = $(event.target);
+			if (!elTarget.length) {
+				return;	
+			}
+			var elScroll;
+			if (elTarget.is(selectorScrollable)) {
+				elScroll = elTarget;
+			} else if ((elScroll = elTarget.parents(selectorScrollable)).length == 0) {
+				elScroll = null;
+			}
+			if (!elScroll) {
+				return;
+			}
+			data.elScroll = elScroll;
+			data.posY = events.pageY;
+			data.scrollY = elScroll.scrollTop();
+			data.maxscroll = elScroll[0].scrollHeight - elScroll[0].clientHeight;
+		},
+		touchmove: function () {
+			if (data.maxscroll <= 0 || isSBBrowser) {
+				event.preventDefault();
+			}
+			var elScroll = data.elScroll;
+			var scrollTop = elScroll.scrollTop();
+			var events = event.touches[0] || event;
+			var distanceY = events.pageY - data.posY;
+			if (isSBBrowser) {
+				elScroll.scrollTop(data.scrollY - distanceY);
+				elScroll.trigger('scroll');
+				return;
+			}
+			if (distanceY > 0 && scrollTop == 0) {
+				event.preventDefault();
+				return;
+			}
+			if (distanceY < 0 && (scrollTop + 1 >= data.maxscroll)) {
+				event.preventDefault();
+				return;
+			}
+		},
+		touchend: function () {
+			data.maxscroll = 0;
+		}	
+	});
+	container.data('isBindScroll', true);
+};
