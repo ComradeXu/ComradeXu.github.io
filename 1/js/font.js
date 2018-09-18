77e7d580b38c1f7b10cd30e6978d16a6082b4e60
comradexu <876581792@qq.com>
2018-09-18 10:36:19 +0800

    remove test

--- 1/js/font.js
+++
@@ -1,1 +0,0 @@
-!function(w,d,m){function r(){var t=100,o=750,e=d.documentElement.clientWidth||w.innerWidth,n=m.max(m.min(e,480),320),h=50;320>=n&&(h=m.floor(n/o*t*.99)),n>320&&362>=n&&(h=m.floor(n/o*t*1)),n>362&&375>=n&&(h=m.floor(n/o*t*1)),n>375&&(h=m.floor(n/o*t*.97)),d.querySelector("html").style.fontSize=h+"px"};r();w.onresize=function(){r()}}(window,document,Math);
--- 1/js/share.js
+++
@@ -1,1 +0,0 @@
-﻿with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?cdnversion='+~(-new Date()/36e5)];
